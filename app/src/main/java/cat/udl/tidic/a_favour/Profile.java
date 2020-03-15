b8afe06990277283f557ede788e3f5a402a83a80
Oriol Figueras <urijunki22@outlook.com>
2020-03-15 14:35:41 +0100

    provan aixo

---
+++ app/src/main/java/cat/udl/tidic/a_favour/Profile.java
@@ -0,0 +1,19 @@
+package cat.udl.tidic.a_favour;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Profile extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+
+
+}
