b17a9fa790ca1cea65cfc8000e66deb9292d20a0
Junki22 <ory__22@hotmail.com>
2020-03-29 23:06:32 +0200

    Nomès funciona amb l'usuari1, pero ja funciona agafant el token de les SharedPreferences. Falta mirar-se si el valor que agafa de la base de dades es null per a que funcioni amb tots els usuaris

--- app/src/main/java/cat/udl/tidic/a_favour/models/ProfileViewModel.java
+++ app/src/main/java/cat/udl/tidic/a_favour/models/ProfileViewModel.java
@@ -54,11 +54,11 @@ public class ProfileViewModel
    {
        Map<String, String> map = new HashMap<>();
        Log.d("IS TOKEN EMPTY? TOKEN = ", mPreferences.getString("token", ""));
-       //map.put("Authorization", mPreferences.getString("token", ""));
+       map.put("Authorization", mPreferences.getString("token", ""));
 
        //Esta hardcodejat perque falta fer la gestió d'errors al agafar els valors
        //De moment mostra les dades de l'usuari 1
-       map.put("Authorization", "656e50e154865a5dc469b80437ed2f963b8f58c8857b66c9bf");
+       //map.put("Authorization", "656e50e154865a5dc469b80437ed2f963b8f58c8857b66c9bf");
 
        userService = RetrofitClientInstance.getRetrofitInstance().create(UserServices.class);
        Call<UserModel> call = userService.getUserProfile(map);
